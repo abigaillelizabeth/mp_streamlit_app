aa90ac5069d68a8867d327f7a5fc3e3ffb44a6fb
abigaillelizabeth <aec92605@uga.edu>
2025-05-30 10:21:39 -0400

    ^^

--- streamlit_doc_conversion.py
+++ streamlit_doc_conversion.py
@@ -364,15 +364,15 @@ def authenticate(username, password):
     # if username == st.secrets["general"]["username"] and password == st.secrets["general"]["password"]:
     #     return True
     # return False
-    
+
     # Debug: print the secrets to see if it is loaded correctly
-    print(st.secrets)  # This will print the entire secrets object to the logs
-    if "credentials" not in st.secrets:
-        st.error("Credentials key is missing from secrets!")
-        return False
+    # print(st.secrets)  # This will print the entire secrets object to the logs
+    # if "credentials" not in st.secrets:
+    #     st.error("Credentials key is missing from secrets!")
+    #     return False
 
     # Loop through the credentials to match the username and password
-    for user in st.secrets["credentials"]:  # Access the list of users
+    for user in st.secrets["credentials"]["user"]:
         if user["username"] == username and user["password"] == password:
             return True  # Successful authentication
     return False  # Failed authentication
